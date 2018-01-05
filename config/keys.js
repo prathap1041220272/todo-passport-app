eeed51b04df5c55e0591ce4fdcca47ed8b3dc17a
prathap1041220272 <sivaprathap.konduru@gmail.com>
2018-01-05 20:06:57 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,12 +3,12 @@
 
 module.exports = {
 	google : {
-		clientID: '774195333121-i09cilard2jem1nibhgffj0nuvo7cjm5.apps.googleusercontent.com',
-    clientSecret: '9bOERbo2_xDGTXeoS_er1dx9'
+		clientID: '774195333121-i09cilard2jem1nibhgffj0nuvo7cjm5ksp.apps.googleusercontent.com',
+    clientSecret: '9bOERbo2_xDGTXeoS_er1dx9ksp'
 	},
 	facebook : {
 		clientID : '229899360884149',
-		clientSecret : '31b566215fac2626daa38ab657a173a9'
+		clientSecret : '31b566215fac2626daa38ab657a173a9ksp'
 	}
 
 }
