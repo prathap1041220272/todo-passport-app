4373ec920f9f5ff9c1e5e258125b996497acf234
prathap1041220272 <sivaprathap.konduru@gmail.com>
2018-02-06 11:32:01 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,17 +1,30 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = 'mongodb://localhost/todo';
-const useMongoClient = true;
-mongoose.Promise = global.Promise;
-mongoose.connect(uri, {useMongoClient}, (err)=> {
-	if (err) 
-		console.log('Database not connected');
+const dbURI = 'mongodb://localhost/test';
 
-     else
-		console.log('Database  connected')
-  
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose default connection open to ' + dbURI);
 })
+
+mongoose.connection.on('error', (err) => {
+	console.log('Mongoose default error ' + err);
+})
+
+mongoose.connection.on('disconnected', (err) => {
+	console.log('Mongoose default disconnected ');
+})
+
+process.on('SIGINT', () => {
+	mongoose.onnection.close(() => {
+	console.log('Mongoose default connected ')
+      process.exit(0);
+	})
+})
+
 
 
 module.exports = mongoose
